67aa7dd682c92c44322c7b2e372c53edf56e6d9e
szgturnin <as65011371@gmail.com>
2019-03-27 19:37:49 +0800

    init

---
+++ FixScan/save.h
@@ -0,0 +1,29 @@
+#ifndef SAVE_H
+#define SAVE_H
+/*
+An interface corresponding to a "Save" button
+*/
+#include <QDialog>
+#include <QByteArray>
+namespace Ui {
+class save;
+}
+
+class save : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit save(QWidget *parent = nullptr);
+    ~save();
+signals:
+	void saveSignal(QByteArray);//The mainwindow receive this signal
+private slots:
+    void on_pushButton_clicked();
+	void on_pushButton_Path_clicked();
+private:
+    Ui::save *ui;
+	bool typeBool(QString type);//Get boolean values corresponding to characters.
+};
+
+#endif // SAVE_H
